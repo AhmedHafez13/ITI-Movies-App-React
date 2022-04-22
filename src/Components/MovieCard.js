ee1c017d242eeb141cc2b67a543d5e98ee87938c
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-04-22 17:05:47 +0200

    Add animation cards css style

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -10,44 +10,46 @@ function MovieCard(props) {
   const { id, title, overview, poster_path, vote_average } = props.movieData;
 
   return (
-    <Card sx={{ borderRadius: 3 }} elevation={0} className="hand-cursor">
-      <Link to={"/movie/" + id}>
-        <ImageListItem>
-          <img className="scalable-image"
-            height='100%'
-            src={"https://image.tmdb.org/t/p/w500/" + poster_path}
-            srcSet={"https://image.tmdb.org/t/p/w500/" + poster_path}
-            alt={title}
-            loading="lazy"
-          />
+    <div className="app-card">
+      <Card elevation={0} className="hand-cursor">
+        <Link to={"/movie/" + id}>
+          <ImageListItem>
+            <img className="scalable-image"
+              height='100%'
+              src={"https://image.tmdb.org/t/p/w500/" + poster_path}
+              srcSet={"https://image.tmdb.org/t/p/w500/" + poster_path}
+              alt={title}
+              loading="lazy"
+            />
 
-          <ImageListItemBar position="top"
-            title={<Typography sx={{ fontSize: '1rem' }}>{title}</Typography>}
-            sx={{
-              background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-                'rgba(0,0,0,0.7) 50%, rgba(0,0,0,0) 110%)'
-            }}
-            actionPosition="left"
-            actionIcon={
-              <IconButton
-                sx={{ color: 'white' }}
-                aria-label={`star ${title}`}>
-                <StarBorderIcon />
-              </IconButton>
-            }
-          />
+            <ImageListItemBar position="top"
+              title={<Typography sx={{ fontSize: '1rem' }}>{title}</Typography>}
+              sx={{
+                background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                  'rgba(0,0,0,0.7) 50%, rgba(0,0,0,0) 110%)'
+              }}
+              actionPosition="left"
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'white' }}
+                  aria-label={`star ${title}`}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+            />
 
-          <Box sx={{ position: 'absolute', top: 35, right: 5, zIndex: 1 }}>
-            <RatingCirle rating={vote_average} />
-          </Box>
+            <Box sx={{ position: 'absolute', top: 35, right: 5, zIndex: 1 }}>
+              <RatingCirle rating={vote_average} />
+            </Box>
 
-          <Box className="card-desc"
-            sx={{ position: 'absolute', bottom: 0, right: 0, left: 0, top: 0, zIndex: 10 }}>
-            <Typography textAlign='center' height='100%' fontSize={2}>{overview}</Typography>
-          </Box>
-        </ImageListItem>
-      </Link>
-    </Card>
+            <Box className="card-desc"
+              sx={{ position: 'absolute', bottom: 0, right: 0, left: 0, top: 0, zIndex: 10 }}>
+              <Typography textAlign='center' height='100%' fontSize={2}>{overview}</Typography>
+            </Box>
+          </ImageListItem>
+        </Link>
+      </Card>
+    </div>
   );
 }
 
